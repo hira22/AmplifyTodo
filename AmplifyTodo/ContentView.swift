813d297674cf5810cbce38a2c41ad667206b09b7
hira22 <masayuki.hiraoka@ietty.co.jp>
2020-08-10 13:36:22 +0900

    Initial Commit

---
+++ AmplifyTodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AmplifyTodo
+//
+//  Created by hiraoka on 2020/08/10.
+//  Copyright © 2020 hiraoka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
